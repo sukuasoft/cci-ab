7d7b005c326d4cd46ba0d503caa799892aeb9570
Sebastião Sukuakueche <sukuakueches@gmail.com>
2026-03-19 20:45:33 +0100

    feat: added tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,21 @@
 function App() {
 
   return (
-    <>
-    
-    </>
+    <div className="px-8 py-10 flex gap-4 ">
+      <div className="w-full">
+          <h1 className="text-4xl font-bold">Estaremos disponíveis em breve</h1>
+          <p  className="text-2xl">Estamos a trabalhar no nosso novo site. Volte brevemente!</p>
+
+          <div>
+            <p>Esteja atualizado!</p>
+               <input type="text" placeholder="Seu email" />
+          <button>Enviar</button>
+          </div>
+      </div>
+      <div className="w-full">
+        Image
+      </div>
+    </div>
   )
 }
 
